1611e6af01ae25181410e37b571af91fff78b880
Landie <landsurfer2000@gmail.com>
2026-08-06 06:29:16 -0700

    remove unused experimental fix code

--- src/draggable/DraggableState.svelte.ts
+++ src/draggable/DraggableState.svelte.ts
@@ -8,22 +8,17 @@ class DragRoot {
 	items: itemType[] = []
 	zoneTag = $state<string | null>(null)
 	zoneId = $state<string | null>(null)
-  /**... an item in the items array, contains a list of items, indicating nested itemss */
-  nestedItems = $state<boolean>(false)
 	parentZoneState = $state<DragRoot | null>(null)
-	/**used for listing parent nested zones, useful to make sure you cant drag an item inside itself. */
-	parentZoneIds = $state<string[]>([])
-	// parentZoneIds = $derived.by(() => {})
 	constructor(items: itemType[], parentDraggableState: DragRoot | undefined) {
 		console.log('building new zone with these items:', $state.snapshot(items))
     this.items = items
 		parentDraggableState ? (this.parentZoneState = parentDraggableState) : (this.parentZoneState = null)
 
 		if (this.parentZoneState !== null && this.parentZoneState.zoneId !== null) {
-			this.parentZoneIds = [...this.parentZoneState.parentZoneIds, this.parentZoneState.zoneId]
-      //find the item that contains the same items list and copy it's id for the zone id
-      //this will be used to figure out if it's dragging inside itself, because the zone id becomes based off the item id.
-      console.log('begin search')
+      //in the parent, finds the item that contains the list that this zone is being created from,
+      //and gets the id of the item to make this new drag root's zone it's id.
+
+      //this is used to check if an item is dragging inside itself later.
       for (const item of this.parentZoneState.items) {
         if (!('list' in item)) continue
         if (item.list === items) this.zoneId = item.id
